297fc3d4e73fa06861140b030a240fda1427c78f
Seiji Noda <m230921@dac.unicamp.br>
2023-03-14 15:54:45 -0300

    aula lab 02: add instanciacoes na Main.java

--- src/mc322/Main.java
+++ src/mc322/Main.java
@@ -3,8 +3,12 @@ package mc322;
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente c1 = new Cliente("Seiji", "059.272.649-56", "14/01/2004", 19, "Campinas - SP");
-		System.out.println(c1.toString());
-		System.out.println(c1.validarCPF(c1.getCpf()) + "");
+		Cliente cli1 = new Cliente("Seiji", "059.272.649-56", "14/01/2004", 19, "Campinas - SP");
+		Veiculo vei1 = new Veiculo("EVR-8404", "Astra", "Chevrolet");
+		Sinistro sin1 = new Sinistro("14/03/2023", "Curitiba - PR");
+		Seguradora seg1 = new Seguradora("Seguros do Jose", "46801563", "segjose@gmail.com", "Campinas - SP");
+		
+		System.out.println("ID do sinsitro: " + sin1.getId());
 	}
 }
+	
