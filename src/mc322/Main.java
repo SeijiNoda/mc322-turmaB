60e2cd542f8819232bc734fea28e4fe02131d361
Seiji Noda <m230921@dac.unicamp.br>
2023-03-28 23:10:43 -0300

    improve main and add signup and remove on seguradora

--- src/mc322/Main.java
+++ src/mc322/Main.java
@@ -1,9 +1,12 @@
 package mc322;
 
+import java.text.DateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
 import mc322.cliente.Cliente;
+import mc322.cliente.ClientePF;
+import mc322.cliente.ClientePJ;
 import mc322.seguradora.Seguradora;
 import mc322.sinistro.Sinistro;
 import mc322.veiculo.Veiculo;
@@ -25,47 +28,130 @@ public class Main {
 		System.out.println("Data: " + sin1.getData());
 		System.out.println("Endereco: " + sin1.getEndereco() + "\n");
 		
-		Seguradora seg1 = new Seguradora("Seguros do Jose", "46801563", "segjose@gmail.com", "Campinas - SP");
-		System.out.println("Nome: " + seg1.getNome());
-		System.out.println("Telefone: " + seg1.getTelefone());
-		System.out.println("Email: " + seg1.getEmail());
-		System.out.println("Endereco: " + seg1.getEndereco() + "\n");*/
+		*/
+		Seguradora seguradora = new Seguradora("Seguros do Jose", "46801563", "segjose@gmail.com", "Campinas - SP");
+//		System.out.println("Nome: " + seg1.getNome());
+//		System.out.println("Telefone: " + seg1.getTelefone());
+//		System.out.println("Email: " + seg1.getEmail());
+//		System.out.println("Endereco: " + seg1.getEndereco() + "\n");
 		
 		Scanner reader = new Scanner(System.in);
 		String comando;
 		do {
-			System.out.println("========== Seguradora ==========");
-			System.out.println("1. [cadastrar] Cadastrar cliente");
-			System.out.println("2. [remover] Remover um sinistro");
-			System.out.println("3. [sinistro] Criar um sinistro");
-			System.out.println("4. [sair] Para sair do programa");
+			System.out.println("=========== Seguradora ===========");
+			System.out.println("1. [clientes] Clientes");
+			System.out.println("2. [sinistros] Sinistros");
+			System.out.println("3. [sair] Sair do programa");
 			comando = reader.nextLine();
 			
 			switch(comando.toLowerCase()) {
 				case "sair": break;
-				case "cadastrar": 
-					System.out.print("Nome: ");
-					String nome = reader.nextLine();
-					
-					System.out.print("Endereco: ");
-					String endereco = reader.nextLine();
-					
-					Date dataLicensa = new Date();
-				
-					System.out.print("Pessoa Fisica ou Juridica? [PF/PJ]: ");
-					//System.out.println("\n");
-					String tipo = reader.next();
-					System.out.print(tipo);
-//					while (!(tipo.toUpperCase().equals("PF") | tipo.toUpperCase().equals("PJ"))) {
-//						System.out.print("Pessoa Fisica ou Juridica? [PF/PJ]: ");
-//						tipo = reader.next();
-//					}
-					
+				case "clientes":
+					String comandoCli = "";
+					do {
+						System.out.println("============ Clientes ============");
+						System.out.println("1.1 [cadastrar] Cadastrar cliente");
+						System.out.println("1.2 [remover] Remover um cliente");
+						System.out.println("1.3 [listar] Listar Clientes");
+						System.out.println("1.4 [voltar] Voltar");
+						comandoCli = reader.nextLine();
+						
+						switch(comandoCli.toLowerCase()) {
+						case "voltar": break;
+						case "cadastrar": {
+							System.out.print("Nome: ");
+							String nome = reader.nextLine();
+							
+							System.out.print("Endereco: ");
+							String endereco = reader.nextLine();
+							
+							Date dataLicensa = new Date();
+						
+							String tipo = "";
+							while (!(tipo.toUpperCase().equals("PF") | tipo.toUpperCase().equals("PJ"))) {
+								System.out.print("Pessoa Fisica ou Juridica? [PF/PJ]: ");
+								tipo = reader.nextLine();
+							}
+							
+							if (tipo.toUpperCase().equals("PF")) {
+								System.out.print("Educacao: ");
+								String educacao = reader.nextLine();
+								
+								System.out.print("Genero: ");
+								String genero = reader.nextLine();
+								
+								System.out.print("Classe Economica: ");
+								String classeEconomica = reader.nextLine();
+								
+								System.out.print("CPF: ");
+								String cpf = reader.nextLine();
+								
+								//System.out.print("Aniversario [dd/mm/yyyy]: ");
+								//String dataNascimentoString = reader.nextLine();
+								Date dataNascimento = new Date();
+							
+								ClientePF novoCliente = new ClientePF(nome, endereco, dataLicensa, educacao, genero, classeEconomica, cpf, dataNascimento);
+								if (seguradora.cadastrarCliente(novoCliente)) {
+									System.out.println("\nMENSAGEM: " + nome + "cadastrado com sucesso!\n");
+								} else {
+									System.out.println("\nMENSAGEM: Jah existe cliente com CPF " + cpf + ".\n");
+								}
+							} else {
+								System.out.print("CNPJ: ");
+								String cnpj = reader.nextLine();
+								
+								// concertar
+								Date dataFundacao = new Date();
+								
+								ClientePJ novoCliente = new ClientePJ(nome, endereco, dataLicensa, "N/A", "N/A", "N/A", cnpj, dataFundacao);
+								if (seguradora.cadastrarCliente(novoCliente)) {
+									System.out.println("\nMENSAGEM: " + nome + "cadastrado com sucesso!\n");
+								} else {
+									System.out.println("\nMENSAGEM: Jah existe cliente com CNPJ " + cnpj + ".\n");
+								}
+							}
+							
+							break;
+						}
+						case "remover": {
+							String tipo = "";
+							while (!(tipo.toUpperCase().equals("PF") | tipo.toUpperCase().equals("PJ"))) {
+								System.out.print("Pessoa Fisica ou Juridica? [PF/PJ]: ");
+								tipo = reader.nextLine();
+							}
+							
+							if (tipo.toUpperCase().equals("PF")) {
+								System.out.print("CPF do cliente a remover: ");
+								String cpf = reader.nextLine();
+								
+								ClientePF remover = new ClientePF("", "", new Date(), "", "", "", cpf, new Date());
+								
+								if (seguradora.removerCliente(remover)) {
+									System.out.println("\nMENSAGEM: Cliente de CPF " + cpf + " removido com sucesso!\n");
+								} else {
+									System.out.println("\nMENSAGEM: Cliente de CPF " + cpf + " nao encontrado.\n");
+								}
+							} else {
+								System.out.print("CNPJ do cliente a remover: ");
+								String cnpj = reader.nextLine();
+								
+								ClientePJ remover = new ClientePJ("", "", new Date(), "", "", "", cnpj, new Date());
+								
+								if (seguradora.removerCliente(remover)) {
+									System.out.println("\nMENSAGEM: Cliente de CNPJ " + cnpj + " removido com sucesso!\n");
+								} else {
+									System.out.println("\nMENSAGEM: Cliente de CNPJ" + cnpj + " nao encontrado.\n");
+								}
+							}
+							break;
+						}
+						case "listar":
+							break;
+					}
+					} while (!comandoCli.toLowerCase().equals("voltar"));
+							
 					break;
-				case "remover":
-					break;
-				case "sinsitro":
-					break;
+				case "sinistros": break;
 			}
 		} while (!comando.toLowerCase().equals("sair"));
 		reader.close();
--- src/mc322/seguradora/Seguradora.java
+++ src/mc322/seguradora/Seguradora.java
@@ -1,16 +1,26 @@
 package mc322.seguradora;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import mc322.cliente.*;
+import mc322.sinistro.Sinistro;
+
 public class Seguradora {
 	private String nome;
 	private String telefone;
 	private String email;
 	private String endereco;
+	private List<Sinistro> listaSinistros; 
+	private List<Cliente> listaClientes;
 	
 	public Seguradora(String nome, String telefone, String email, String endereco) {
 		this.nome = nome;
 		this.telefone = telefone;
 		this.email = email;
 		this.endereco = endereco;
+		this.listaSinistros = new ArrayList<Sinistro>();
+		this.listaClientes = new ArrayList<Cliente>();
 	}
 	
 	public String getNome() {
@@ -43,5 +53,59 @@ public class Seguradora {
 
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
+	}
+
+	public List<Sinistro> getListaSinistros() {
+		return listaSinistros;
+	}
+
+	public void setListaSinistros(List<Sinistro> listaSinistros) {
+		this.listaSinistros = listaSinistros;
+	}
+
+	public List<Cliente> getListaClientes() {
+		return listaClientes;
+	}
+
+	public void setListaClientes(List<Cliente> listaClientes) {
+		this.listaClientes = listaClientes;
+	}
+	
+	public boolean cadastrarCliente(Cliente novo) {
+		for(Cliente cliente: this.listaClientes) {
+			if (novo instanceof ClientePF && cliente instanceof ClientePF) {
+				
+				boolean cpfIgual = ((ClientePF) novo).getCpf().equals(((ClientePF) cliente).getCpf());
+				if (cpfIgual) return false;
+			} else if (novo instanceof ClientePJ && cliente instanceof ClientePJ) {
+				
+				boolean cnpjIgual = ((ClientePJ) novo).getCnpj().equals(((ClientePJ) cliente).getCnpj());
+				if (cnpjIgual) return false;
+			}
+		}
+		
+		this.listaClientes.add(novo);
+		return false;
+	}
+	
+	public boolean removerCliente(Cliente remover) {
+		for(Cliente cliente: this.listaClientes) {
+			if (remover instanceof ClientePF && cliente instanceof ClientePF) {
+				
+				boolean cpfIgual = ((ClientePF) remover).getCpf().equals(((ClientePF) cliente).getCpf());
+				if (cpfIgual) {
+					this.listaClientes.remove(cliente);
+					return true;
+				}
+			} else if (remover instanceof ClientePJ && cliente instanceof ClientePJ) {
+				
+				boolean cnpjIgual = ((ClientePJ) remover).getCnpj().equals(((ClientePJ) cliente).getCnpj());
+				if (cnpjIgual) {
+					this.listaClientes.remove(cliente);
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
